2f9fe6cdadf6eaa49b5a3a8b3335e720d98a10bd
Mario <mariodominguez1809@gmail.com>
2024-02-20 10:40:32 -0600

    Actualizacion

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,32 @@ class UsersController extends Controller
 {
     //consultar todos los Usuarios
     public function index(){
-        return User::all();
+        return User::paginate();
     }
+    //consultar un Usuario
+    public function show($id){
+        return User::find($id);
+    }
+
+    //crear un Usuario
+    public function store(Request $request){
+        return User::create($request->all());
+    }
+
+    //actualizar un Usuario
+    public function update(Request $request,$id){
+        $user = User::find($id);
+        $user->update($request->all());
+        $user->save();
+        return $user;
+    }
+
+    //eliminar un Usuario
+    public function destroy($id){
+        $user = User::find($id);
+        $user->delete();
+        return $user;
+    }
+
+
 }
